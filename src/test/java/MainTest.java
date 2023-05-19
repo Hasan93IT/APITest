0facc539ba084cdbd543f0a8ec639d711ce9175c
Hasan Abdullah <89301568+Hasan93IT@users.noreply.github.com>
2023-05-19 19:14:19 +0200

    test new sides

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,12 +5,34 @@ import static io.restassured.RestAssured.given;
 public class MainTest {
 
 
-
+    @Test
+    public void TestMainSideDeutsch() {
+        given().
+                get("https://www.amjis.com").
+                then().
+                statusCode(200);
+    }
 
     @Test
-    public void TestAPI02() {
+    public void TestMainSideEnglish() {
         given().
-                get("https://www.amjis.com/").
+                get("https://www.amjis.com/?language=en").
+                then().
+                statusCode(200);
+    }
+
+    @Test
+    public void TestMainSideContactDE() {
+        given().
+                get("https://www.amjis.com/contact.php?language=de").
+                then().
+                statusCode(200);
+    }
+
+    @Test
+    public void TestMainSideContactEN() {
+        given().
+                get("https://www.amjis.com/contact.php?language=en").
                 then().
                 statusCode(200);
     }
