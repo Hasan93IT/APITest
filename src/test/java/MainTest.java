972a68cfd204eba2027aa1d3650bf4bbdec647d0
Hasan Abdullah <89301568+Hasan93IT@users.noreply.github.com>
2023-05-19 18:40:43 +0200

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,19 +5,13 @@ import static io.restassured.RestAssured.given;
 public class MainTest {
 
 
-    @Test
-    public void TestAPI1() {
-        given().
-                get("https://reqres.in/api/users?page=2").
-                then().
-                statusCode(200);
-    }
+
 
     @Test
     public void TestAPI02() {
         given().
                 get("https://www.amjis.com/").
                 then().
-                statusCode(201);
+                statusCode(200);
     }
 }
